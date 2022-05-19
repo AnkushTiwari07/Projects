84cc8f48ab586f9c86f55eae49319e215d15fa4b
Ankush <ankusht40@gmail.com>
2022-05-19 22:23:37 +0530

    Ignore

---
+++ src/practicse/ab.java
@@ -0,0 +1,30 @@
+package practicse;
+
+public class ab {
+	
+	public static void main(String[] args) {
+		
+		int var = 1221;
+		int tempvar = 0;
+		
+		for(int i = 4 ; i >=0; i--) {
+			
+			
+			tempvar = tempvar*10+var;
+			
+			
+		}
+		
+		System.out.println(tempvar);
+		
+	}
+	
+		
+	}
+
+		
+			
+
+
+
+
